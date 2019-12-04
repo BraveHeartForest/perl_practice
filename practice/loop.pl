65557204964108e636b2d9ddb4086752b6c8db83
BraveHeartForest <kapegyonyu@svk.jp>
2019-12-04 14:19:05 +0900

    大幅に改稿しました

--- practice/loop.pl
+++ practice/loop.pl
@@ -8,29 +8,43 @@ binmode STDOUT, ':encoding(cp932)';
 binmode STDERR, ':encoding(cp932)';
 
 for (my $i = 0; $i < 10; $i++) {
-  print "$i\t";
+    print "$i\t";
 }
 print "\n";
 for (my $i = 0; $i < 10; ++$i) {
-  print "$i\t";
+    print "$i\t";
 }
 print "\n";
 for (my $i = 0; $i <= 10; $i++) {
-  print "$i\t";
+    print "$i\t";
 }
 print "\n";
 for (my $i = 0; $i < 10; $i+=2) {
-  print "$i\t";
+    print "$i\t";
 }
 print "\n";
 
 my $n = shift;  #コマンドラインから引数を取得する。
+my $primeFlag = 0;
 if ($n == 1) {
-    print "1は素数ではありません。\n" and exit;
-}
-else {
+    print "1は素数ではありません。\n" and last;
+} else {
     for my $i (2 .. sqrt $n){
-        print "${n}は素数ではありません。\n" and exit unless($n % $i);
+        if ($n % $i == 0) {
+            $primeFlag++;   # nをiで割ったときの余りが0の場合に+1する。
+        }
     }
-    print "${n}は素数です。\n";
+    if ($primeFlag > 0) {
+        print "${n}は素数ではありません。\n";
+    }else{
+        print "${n}は素数です。\n";
+    }
 }
+
+# whileの使いどころさん。
+my @animals = ('Cat', 'Dog', 'Mouse');
+print @animals."\n";    # 3
+while (my $animal = pop @animals) {
+    print "$animal\n";
+}
+print @animals."\n";    # 0
