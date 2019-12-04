6628b56e280fda6d567d56493f79c336abbc843c
BraveHeartForest <kapegyonyu@svk.jp>
2019-12-04 10:51:45 +0900

    コメント追加

--- practice/refHash3.pl
+++ practice/refHash3.pl
@@ -17,7 +17,7 @@ while (my $line = <>) {
 
     # (2)各時刻のデータを保存するための配列のリファレンス
     $infos->{$time} ||= [];
-    # $infos->{$time}が偽の時（undef)に []を設定する。
+    # $infos->{$time}が偽の時（undef)に []（＝配列のリファレンス）を設定する。
 
     # (3)件数の合計
     $infos->{$time}[0]++;
@@ -28,4 +28,25 @@ while (my $line = <>) {
     # (5)最大応答時間
     $infos->{$time}[2] = $res_time
         if !defined $infos->{$time}[2] || $res_time > $infos->{$time}[2];
+    # $infos->{$time}{2}が未定義、若しくは$res_timeが現在の$infos->{$time}{2}よりも大きいならば$infos->{$time}{2}に$res_timeを代入する。
+    # 結果的に$infos->{$time}{2}には全ての$res_timeの内で最大のものが格納される。
 }
+
+# 完成図
+# my $infos = {
+#     '01:03' => [
+#          4,
+#          20,
+#          9
+#    ],
+#     '01:01' => [
+#          2,
+#          5,
+#          3
+#    ],
+#     '01:02' => [
+#          3,
+#          10,
+#          5
+#      ]
+# };
